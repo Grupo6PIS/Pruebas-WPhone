39706d37af016e2acefc3c2d2da58dbf6a4334c8
MBerguer <popperismo@gmail.com>
2014-09-18 00:34:14 -0300

    Pruebas Unitarias

---
+++ BeatItTest/Test.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BeatIt_.AppCode.Challenges;
+using BeatIt_.AppCode.Controllers;
+using BeatIt_.AppCode.Interfaces.Controllers;
+using BeatIt_;
+
+//ojo
+
+namespace BeatItTest
+{
+    [TestClass]
+    public class TestingUb
+    {
+        [TestMethod]
+        public void TestingFunctioncalculatePuntaje()
+        {
+            Assert.AreEqual(4, 2 + 2);
+
+        }
+    };
+};
+
+
