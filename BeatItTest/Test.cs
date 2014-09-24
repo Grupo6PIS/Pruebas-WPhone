105ecac6db5494cdfb1f680e819588facda46d1a
MBerguer <popperismo@gmail.com>
2014-09-23 22:51:02 -0300

    Funka?

--- BeatItTest/Test.cs
+++ BeatItTest/Test.cs
@@ -15,8 +15,8 @@ namespace BeatItTest
         {
             Assert.AreEqual(4,2+2);
             
-            //IChallengeController ich = ChallengeController.getInstance();
-            //UsainBolt ubBolt = (UsainBolt)ich.getChallenge(BeatIt_.AppCode.Enums.ChallengeType.CHALLENGE_TYPE.USAIN_BOLT);
+            IChallengeController ich = ChallengeController.getInstance();
+            UsainBolt ubBolt = (UsainBolt)ich.getChallenge(BeatIt_.AppCode.Enums.ChallengeType.CHALLENGE_TYPE.USAIN_BOLT);
 
             // ACÁ se tiene que probar la función de puntaje, 
             // el tema es que no está creada la función de almacenar el puntaje
